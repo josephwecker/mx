760281d8e38777acd65eb467f921f40615662f23
Joseph Wecker <jwecker@justin.tv>
2011-12-02 17:47:33 -0800

    Started recursive-descent, but will need to do a quick genmachine statemachine if I want to have any hope of comments.

--- src/subjson.c
+++ src/subjson.c
@@ -22,15 +22,41 @@
 #include <sysexits.h>
 #include <err.h>
 
+#define UNPACK_STATE()       uint64_t *qcurr = state->p_quick; \
+                             char     *curr  = state->p_curr;  \
+                             uint64_t *qeof  = state->p_qend;  \
+                             char     *eof   = state->p_end;
+#define COMMIT_STATE()       state->p_quick = qcurr; state->p_curr = curr
+#define discard_and_commit() curr++; COMMIT_STATE();
+
+// Modified from http://graphics.stanford.edu/~seander/bithacks.html#ZeroInWord
+#define q_haszero(v)         ((v) - UINT64_C(0x0101010101010101)) & ~(v) & UINT64_C(0x8080808080808080)
+#define q_hasval(v,n)        (q_haszero((v) ^ (~UINT64_C(0)/255 * (n))))
+
+// Search for one of two characters- first big chunks at a time and then get
+// the actual byte. (First statement resets qcurr to whatever curr is so curr
+// is authoritative).
+#define qscan2(c1,c2)        qcurr=(uint64_t *)curr; \
+                             while((qcurr <= qeof) && !q_hasval(*qcurr,(c1)) && !q_hasval(*qcurr,(c2)))\
+                                 qcurr++; \
+                             curr=(char *)qcurr;     \
+                             while((curr <= eof) && (*curr != c1) && (*curr != c2))\
+                                 curr++;
+
+
 
 typedef struct {
     char      *filename;
     int        fd;
     ssize_t    size;
+    ssize_t    qsize;
 
     char      *p_start;
     uint64_t  *p_quick;
     char      *p_curr;
+
+    char      *p_end;
+    uint64_t  *p_qend;
     // ref_start for current "word" start...
     //
     // structures for holding the results objects...
@@ -52,9 +78,11 @@ pstate *init_from_file(char *filename) {
         err(EX_NOINPUT, "Opened, but couldn't stat %s.", filename);
 
     state->size     = statbuf.st_size;
+    state->qsize    = state->size >> 3; // size in uint64_t chunks
     state->filename = filename;
     
-    if( (state->p_start = (char *) malloc(state->size+2)) == NULL)
+    // padding to the right so that quickscan stuff can look in bigger chunks
+    if( (state->p_start = (char *) malloc(state->size+8)) == NULL)
         err(EX_OSERR, "Couldn't allocate memory for file contents (%s).", filename);
 
     if( (bytes_read = read(fd, state->p_start, state->size)) != state->size)
@@ -62,6 +90,8 @@ pstate *init_from_file(char *filename) {
 
     state->p_curr   = state->p_start;
     state->p_quick  = (uint64_t *) state->p_start;
+    state->p_end    = &(state->p_start[state->size - 1]);
+    state->p_qend   = &(state->p_quick[state->qsize - 1]);
     state->p_curr[state->size] = 0; // Null terminate the whole thing just in case
     close(fd);
     return state;
@@ -75,8 +105,46 @@ void free_state(pstate *state) {
     }
 }
 
-
 int parse(pstate *state) {
+    return toplevel(state);
+}
+
+int toplevel(pstate *state) {
+    UNPACK_STATE();
+    while(curr <= eof) {
+        qscan2('{','[');
+        switch(*curr) {
+            case '{':
+                discard_and_commit();
+                new_dict(state);
+                break;
+            case '[':
+                discard_and_commit();
+                new_list(state);
+                break;
+            default:
+                discard_and_commit();
+        }
+    }
+}
+
+int new_dict(pstate *state) {
+    printf("\n-->DICT\n%s\n", state->p_curr);
+
+}
+
+int new_list(pstate *state) {
+    printf("\n-->LIST\n%s\n", state->p_curr);
+    // - create structure
+    // - next_value* until ']'
+}
+
+int next_value(pstate *state) {
+    // - skipwhites
+    // - could be: explicit-string, list, dict, scalar, nothing(comma),
+    //             nothing(end-bracket)
+    UNPACK_STATE();
+    //skipwhites();
 
 }
 
